cc3f9c3a264fff25c42db3c3852d41b8b60ed492
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-09-23 00:41:45 -0400

    fix args

--- internal/args.go
+++ internal/args.go
@@ -2,18 +2,17 @@ package internal
 
 import "flag"
 
-func Args(chunkSize int, batchSize int, file string) {
-	// TODO: set default by file size
-	flag.IntVar(&chunkSize, "chunk-size", 1_000_000, "Chunk size")
-	flag.IntVar(&batchSize, "batch-size", 10_000, "Batch size")
-	flag.StringVar(&file, "file", "", "File path")
+func Args(chunkSize *int, batchSize *int, filePath *string) {
+	flag.IntVar(chunkSize, "chunk-size", 10_000, "Chunk size")
+	flag.IntVar(batchSize, "batch-size", 1_000, "Batch size")
+	flag.StringVar(filePath, "file-path", "", "File path")
 	flag.Parse()
 
-	if batchSize > chunkSize {
+	if *batchSize > *chunkSize {
 		panic("batch size cannot be bigger than chunk size")
 	}
 
-	if file == "" {
+	if *filePath == "" {
 		panic("invalid file path")
 	}
 }
