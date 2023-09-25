37b6b777a8b0120bb9c3c93d466dd1dd1645c4f5
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-09-25 19:38:15 -0400

    debug flag

--- internal/args.go
+++ internal/args.go
@@ -4,12 +4,14 @@ import "flag"
 
 type Args struct {
 	ConfigPath string
+	Debug      bool
 }
 
 func ParseArgs() Args {
 	var args Args
 
 	flag.StringVar(&args.ConfigPath, "config", "config.yaml", "Config path")
+	flag.BoolVar(&args.Debug, "debug", false, "Debug mode")
 	flag.Parse()
 
 	return args
