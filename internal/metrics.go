3923d757f806a4993364466022d470d8b2c12533
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-09-25 19:38:34 -0400

    refactor: metrics

--- internal/metrics.go
+++ internal/metrics.go
@@ -2,24 +2,12 @@ package internal
 
 import (
 	"fmt"
-	"runtime"
 	"time"
 )
 
 func MeasureExecTime(id string, function func()) {
 	start := time.Now()
 	function()
-	// PrintMemoryUsage() // TODO: Enable with debug flag?
 	duration := time.Since(start)
 	fmt.Printf("%s (%s)\n", id, duration)
-}
-
-func PrintMemoryUsage() {
-	var m runtime.MemStats
-	runtime.ReadMemStats(&m)
-
-	fmt.Printf("Alloc = %v MiB", m.Alloc/1024/1024)
-	fmt.Printf("\tTotalAlloc = %v MiB", m.TotalAlloc/1024/1024)
-	fmt.Printf("\tSys = %v MiB", m.Sys/1024/1024)
-	fmt.Printf("\tNumGC = %v\n", m.NumGC)
 }
