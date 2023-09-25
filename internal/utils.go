4259418e443d202fd900ca4e8d693d6a4c93a04b
Karan Pratap Singh <contact@karanpratapsingh.com>
2023-09-25 19:04:45 -0400

    generic

--- internal/utils.go
+++ internal/utils.go
@@ -1,24 +1,8 @@
 package internal
 
-// TODO: make generic
-func copySlice(input [][]string) [][]string {
-	copiedSlice := make([][]string, len(input))
+func copySlice[T any](input [][]T) [][]T {
+	copiedSlice := make([][]T, len(input))
 	copy(copiedSlice, input)
 
 	return copiedSlice
-}
-
-func makeBatches[T any](items []T, batchSize int) [][]T {
-	var batches [][]T
-	N := len(items)
-
-	for i := 0; i < N; i += batchSize {
-		end := i + batchSize
-		if end > N {
-			end = N
-		}
-		batches = append(batches, items[i:end])
-	}
-
-	return batches
 }
